a71495cb8c49fbcf791c661bdbf2c0d9168acad0
NelsonSenhouse <144487032+NelsonSenhouse@users.noreply.github.com>
2024-12-09 13:20:43 -0500

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def get_data(file_name):
     f = open(file_name)
     data = []
@@ -5,23 +8,28 @@ def get_data(file_name):
         data.append(line.rstrip())
     return data
 
-def mult_evaluate(list, indx, start, target):
-    mult = start * int(list[indx + 1])
-    add = start + int(list[indx + 1])
-    if (indx == len(list) - 2):
-        return (mult == target or add == target)
-    return mult_evaluate(list, indx + 1, start, target)
+# def mult_evaluate(list, indx, start, target):
+#     mult = start * int(list[indx + 1])
+#     add = start + int(list[indx + 1])
+#     if (indx == len(list) - 2):
+#         return (mult == target or add == target)
+#     mult_evaluate(list, indx + 1, mult, target)
+#     mult_evaluate(list, indx + 1, add, target)
+#
+# def add_evaluate(list, indx, start, target):
+#     add = start + int(list[indx + 1])
+#     if (indx == len(list) - 2):
+#         print("hello")
+#         return (add == target)
+#     return add_evaluate(list, indx + 1, add, target)
+list = list(itertools.permutations("abc"))
+for l in list:
+    print(l)
 
-def add_evaluate(list, indx, start, target):
-    add = start + int(list[indx + 1])
-    if (indx == len(list) - 2):
-        print("hello")
-        return (add == target)
-    return add_evaluate(list, indx + 1, add, target)
 
 nums = [1, 2, 3]
-print(mult_evaluate(nums, 0, nums[0], 6))
-print(add_evaluate(nums, 0, nums[0], 7))
+for i in range(len(nums)):
+    print(2 ** nums[i])
 
 input = get_data("Input.txt")
 test = []
@@ -34,4 +42,3 @@ for op in ops:
     op_nums.append(op.split(" "))
 print(test)
 print(op_nums)
-
