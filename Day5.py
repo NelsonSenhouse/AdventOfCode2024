b22742dfd3b8875c7e52b82efa089f536ae54e0a
NelsonSenhouse <144487032+NelsonSenhouse@users.noreply.github.com>
2024-12-08 22:35:06 -0500

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -6,6 +6,7 @@ def get_first_data(file_name):
             data.append(line.rstrip().split("|"))
     return data
 
+
 def get_sec_data(file_name):
     f = open(file_name)
     data = []
@@ -14,35 +15,57 @@ def get_sec_data(file_name):
             data.append(line.rstrip().split(","))
     return data
 
+
 dir = get_first_data("Input.txt")
 pgs = get_sec_data("Input.txt")
-# print(dir)
-# print(pgs)
 safe_rows = []
+unsafe_rows = []
 
+# Part 1
 for i in range(len(pgs)):
     safe = True
-    prev = []
     for j in range(len(pgs[i])):
         for k in range(len(dir)):
             for l in range(len(dir[k])):
                 if (dir[k][l] == pgs[i][j]):
-                    print(dir[k])
                     if (l == 0):
-                        if (dir[k][1] in prev):
-                            print("NO")
-                            print(pgs[i])
+                        if (dir[k][1] in pgs[i][0:j]):
                             safe = False
-                            break
                     else:
                         if (dir[k][0] in pgs[i][j:len(pgs[i])]):
-                            print("NO")
-                            print(pgs[i])
                             safe = False
-                            break
-        prev.append(pgs[i][j])
-        if (safe):
-            safe_rows.append(pgs[i])
+        if (not safe):
+            break
+    if (safe):
+        safe_rows.append(pgs[i])
+    else:
+        unsafe_rows.append(pgs[i])
 
-# print(matches)
-print(safe_rows)
+sum = 0
+for row in safe_rows:
+    sum += int(row[int(len(row) / 2)])
+print(sum)
+
+# Part 2
+print(unsafe_rows)
+for i in range(len(unsafe_rows)):
+    for j in range(len(unsafe_rows[i])):
+        for k in range(len(dir)):
+            for l in range(len(dir[k])):
+                if (dir[k][l] == unsafe_rows[i][j]):
+                    if (l == 0):
+                        if (dir[k][1] in unsafe_rows[i][0:j]):
+                            temp = dir[k][1]
+                            unsafe_rows[i][unsafe_rows[i].index(dir[k][1])] = unsafe_rows[i][j]
+                            unsafe_rows[i][j] = temp
+                    else:
+                        if (dir[k][0] in unsafe_rows[i][j:len(unsafe_rows[i])]):
+                            temp = dir[k][0]
+                            unsafe_rows[i][unsafe_rows[i].index(dir[k][0])] = unsafe_rows[i][j]
+                            unsafe_rows[i][j] = temp
+
+print(unsafe_rows)
+sum = 0
+for row in unsafe_rows:
+    sum += int(row[int(len(row) / 2)])
+print(sum)
