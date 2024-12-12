44bf2a3e1cbc76e54498375e793a10914421fcc6
NelsonSenhouse <144487032+NelsonSenhouse@users.noreply.github.com>
2024-12-12 00:37:56 -0500

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -1,6 +1,3 @@
-import itertools
-
-
 def get_data(file_name):
     f = open(file_name)
     data = []
@@ -8,37 +5,42 @@ def get_data(file_name):
         data.append(line.rstrip())
     return data
 
-# def mult_evaluate(list, indx, start, target):
-#     mult = start * int(list[indx + 1])
-#     add = start + int(list[indx + 1])
-#     if (indx == len(list) - 2):
-#         return (mult == target or add == target)
-#     mult_evaluate(list, indx + 1, mult, target)
-#     mult_evaluate(list, indx + 1, add, target)
-#
-# def add_evaluate(list, indx, start, target):
-#     add = start + int(list[indx + 1])
-#     if (indx == len(list) - 2):
-#         print("hello")
-#         return (add == target)
-#     return add_evaluate(list, indx + 1, add, target)
-list = list(itertools.permutations("abc"))
-for l in list:
-    print(l)
+def evaluate(list, indx, start_list, nums_list, target):
+    print(list)
+    print(indx)
+    print(nums_list)
+    print(target)
+    for num in start_list:
+        nums_list.append(num * list[indx + 1])
+        nums_list.append(num + list[indx + 1])
+    if (indx == len(list) - 2):
+        print("done")
+        return nums_list
+    start_list = nums_list
+    #
+    # if (indx == len(list) - 2):
+    #     print("done")
+    #     return nums_list
+
+    return evaluate(list, indx + 1, [2, 3], nums_list, target)
+
 
 
 nums = [1, 2, 3]
-for i in range(len(nums)):
-    print(2 ** nums[i])
+print(evaluate(nums, 0, [nums[0]], [], 2))
 
-input = get_data("Input.txt")
-test = []
-ops = []
-op_nums = []
-for data in input:
-    test.append(data.split(":")[0])
-    ops.append(data.split(":")[1][1:len(data.split(":")[1])])
-for op in ops:
-    op_nums.append(op.split(" "))
-print(test)
-print(op_nums)
+# input = get_data("Input.txt")
+# test = []
+# ops = []
+# op_nums = []
+# for data in input:
+#     test.append(int(data.split(":")[0]))
+#     ops.append(data.split(":")[1][1:len(data.split(":")[1])])
+# for op in ops:
+#     op_nums.append(op.split(" "))
+# for i in range(len(op_nums)):
+#     for j in range(len(op_nums[i])):
+#         op_nums[i][j] = int(op_nums[i][j])
+# print(test)
+# print(op_nums)
+
